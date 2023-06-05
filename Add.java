2d94b55184eb2612fcb92bb09c5e78224318b6a5
Subhalaxmipatra <subhalaxmipatra81@gmail.com>
2023-06-05 12:23:16 +0530

    1st gitcode

---
+++ Add.java
@@ -0,0 +1,16 @@
+public class Add{
+	public static voidmain(String args[]){
+   		int a=223 , b=34;
+		int add = a+b;
+	System.out.println(add);
+	}
+}
+
+
+
+
+
+
+
+	
+ 
---
+++ swapnumber.java
@@ -0,0 +1,9 @@
+public class swapnumber{
+	public static voidmain(String args[]){
+	int a=12,b=32;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("Swap number"'b');
+	}
+}
